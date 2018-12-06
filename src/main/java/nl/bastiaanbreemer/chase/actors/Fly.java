b8de0432efba4a65fd5e09572e62e795e545fad7
badgamernl <badgamernl@gmail.com>
2018-12-06 12:29:31 +0100

    Fly now flies right to left and back in a loop
    * Edit: Fly now flies the whole width of a map in a loop

--- src/main/java/nl/bastiaanbreemer/chase/actors/Fly.java
+++ src/main/java/nl/bastiaanbreemer/chase/actors/Fly.java
@@ -1,6 +1,7 @@
 package nl.bastiaanbreemer.chase.actors;
 
 import nl.bastiaanbreemer.chase.utils.animations.AnimatedMover;
+import nl.bastiaanbreemer.chase.utils.engine.TileEngine;
 
 /**
  * @author R. Springer
@@ -10,19 +11,22 @@ public class Fly extends AnimatedMover {
     private final static String ANIMATION_PATH = "enemy/%NAME%%FRAME%.png";
 
     private Chaser chaser;
-    private int direction = 0;
 
     public Fly(Chaser chaser) {
         super(ANIMATION_PATH);
-        addAnimation("fly", 2, 20);
+        addAnimation("fly", 2, 10);
         setAnimation("fly");
-        velocityX = 1;
+        velocityX = -1;
         this.chaser = chaser;
     }
 
     @Override
     public void act() {
         if (getX() <= 0) {
+            velocityX = 1;
+        } else if (getX() > TileEngine.MAP_WIDTH * TileEngine.TILE_WIDTH) {
+            velocityX = -1;
+        }
         if (velocityX > 0) {
             setMirrorHorizontally(true);
         } else {
@@ -32,7 +36,7 @@ public class Fly extends AnimatedMover {
 
 //        float distance = (float) Math.hypot(getX() - chaser.getX(), getY() - chaser.getY()) / TileEngine.TILE_WIDTH;
 
-        velocityY = (int) (Math.sin(Math.toRadians(getX() * 2)) * 2);
+        velocityY = (int) (Math.sin(Math.toRadians(getX() * 2)) * 4);
 
         applyVelocity();
     }
